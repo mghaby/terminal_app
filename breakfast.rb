38608e67407487fd7ad1f1667854306f0cf2bced
mghaby <mark.ghaby71@hotmail.com>
2020-09-30 21:42:09 +1000

    testing breakfast methods

--- breakfast.rb
+++ breakfast.rb
@@ -1,2 +1,39 @@
+# use this as foundation for the other classes
 class Breakfast
+    attr_accessor :food, :calorie, :breakf, :breakc
+
+    def initialize(food, calorie)
+        @food = food
+        @calorie = calorie
+        @breakf = []
+        @breakc = []
+    end
+    
+    def food_add(foods)
+    @breakf << foods
+    end
+
+    def calorie_add(cals)
+    @breakc = cals
+    end
+    # def initialize(calorie)
+    #     @calorie = calorie
+    # end
+
+    # def add_food(food, calorie)
+    #     @breakf << @food = food
+    # end
+
+    # def add_cal(calorie)
+    #     @breakc << @calorie = calorie
+    # end
+
+    
+
 end
+
+
+# def add_food(food)
+#     test = Breakfast.new(food)
+#     @test = << test
+# end
